746783d40fa894dc054e923c34250c228dfd1289
PeratX <1215714524@qq.com>
2017-09-08 18:10:59 +0800

    Update Fun DNS node

--- app/src/main/java/org/itxtech/daedalus/Daedalus.java
+++ app/src/main/java/org/itxtech/daedalus/Daedalus.java
@@ -55,7 +55,7 @@ public class Daedalus extends Application {
 
     public static final List<DNSServer> DNS_SERVERS = new ArrayList<DNSServer>() {{
         add(new DNSServer("119.23.248.241", R.string.server_fundns_south_china));
-        add(new DNSServer("116.196.87.241", R.string.server_fundns_north_china));
+        add(new DNSServer("116.196.109.54", R.string.server_fundns_north_china));
         add(new DNSServer("115.159.220.214", R.string.server_puredns_east_china));
         add(new DNSServer("123.207.137.88", R.string.server_puredns_north_china));
         add(new DNSServer("115.159.146.99", R.string.server_aixyz_east_china));
